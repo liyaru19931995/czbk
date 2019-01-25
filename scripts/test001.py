27b19e4e4634bc332c75e91bc60fd6d657ac0bb4
liyaru19931995 <1219940788@qq.com>
2019-01-25 21:06:14 +0800

    111

--- scripts/test001.py
+++ scripts/test001.py
@@ -1,10 +1,13 @@
+import allure
 
 class TestJen:
 
+    @allure.step(title='test01的步骤名称')
     def test_01(self):
         print('test01-----')
         assert True
 
+    @allure.step(title='test02的步骤名称')
     def test_02(self):
         print('test02-----')
         assert False
